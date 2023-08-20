7147c9b355c2f960b00165508e6407b1efd5b121
Arnab Pal <arnab.pal.21@aot.edu.in>
2023-08-21 01:28:04 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Card, CardHeader, CardBody, CardFooter ,Text,Image,Stack,Heading,Divider,ButtonGroup,Button} from '@chakra-ui/react'
+import Fade from 'react-reveal/Zoom';
 
 function App() {
   return (
-    <div className="bg-black">
-      <p>Hello</p>
+    <div>
+      
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="aboutus" element={<AboutUs />} />
+            <Route path="contactus" element={<ContactUs />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="login" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <div>  
-    <App />
-  </div>
+
+  <ChakraProvider>  
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
 );
